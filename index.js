94f8817dcd6650d1d1a374fcad00a115dfd400ed
Sonia Mathew <sonia@Sonias-MacBook-Pro.local>
2019-08-03 12:11:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+$(window).scroll(function(e) {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $('.navbar').removeClass("navbar-expand-lg");
+    } else {
+        $('.navbar').addClass("navbar-expand-lg");
+    }
+});
+
+$(document).ready(function(){
+ let scroll_link = $('.scroll');
+
+  //smooth scrolling -----------------------
+  scroll_link.click(function(e){
+      e.preventDefault();
+      let url = $('body').find($(this).attr('href')).offset().top;
+      $('html, body').animate({
+        scrollTop : url
+      },700);
+      $(this).parent().addClass('active');
+      $(this).parent().siblings().removeClass('active');
+      return false;
+   });
+});
